65f3a1f7ff62a097d73666e0bf9721bc6dda0cca
Abbas Momeni <abbasmomeny1994@gmail.com>
2021-08-15 12:23:17 +0430

    write touche and modify User->projects method

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,7 @@ class User extends Authenticatable
 
     public function projects()
     {
-        return $this->hasMany(Project::class);
+        return $this->hasMany(Project::class)
+            ->latest('updated_at');
     }
 }
